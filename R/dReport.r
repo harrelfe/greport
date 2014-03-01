d08237e30c27057b38bc19cf6ef94c961b598a86
harrelfe <f.harrell@vanderbilt.edu>
2014-03-01 08:28:53 -0600

    Creation

---
+++ R/dReport.r
@@ -0,0 +1,373 @@
+#' Descriptive Statistics Report
+#'
+#' Generate graphics and LaTeX with descriptive statistics
+#' 
+#' \code{dReport} generates multi-panel charts, separately for categorical analysis variables and continuous ones.  The Hmisc \code{summaryP} function and its plot method are used for categorical variables, and \code{bpplotM} is used to make extended box plots for continuous ones unless \code{what='byx'}.   Stratification is by treatment or other variables.  The user must have defined a LaTeX macro \code{\\eboxpopup} (which may be defined to do nothing) with one argument.  This macro is called with argument \code{extended box plot} whenever that phrase appears in the legend, so that a \code{PDF} popup may be generated to show the prototype.  See the example in \code{report.Rnw} in the \code{tests} directory.  Similarly a popup macro \code{\\qintpopup} must be defined, which generates a tooltip for the phrase \code{quantile intervals}.
+#'
+#' @param formula a formula accepted by the \code{bpplotM} or \code{summaryP} functions.  \code{formula} must have an \code{id(subjectidvariable)} term if there are repeated measures, in order to get correct subject counts as \code{nobs}.
+#' @param groups a superpositioning variable, usually treatment, for categorical charts.  For continuous analysis variables, \code{groups} becomes the \code{y}-axis stratification variable.  This is a single character string.
+#' @param what \code{"box"} (the default) or \code{"xy"} for continuous analysis variables, or \code{"proportions"} (or shorter) for categorical ones.  Instead, specifying \code{what="byx"} results in an array of quantile intervals for continuous \code{y}, Wilson confidence intervals for proportions when \code{y} is binary, or means and parametric confidence limits when \code{y} is not continuous but is not binary.  If \code{what} is omitted or \code{what="byx"}, actions will be inferred from the most continuous variable listed in \code{formula}.  When \code{fun} is given, different behavior results (see below).
+#' @param byx.type set to \code{"quantiles"} to show vertical quantile intervals of \code{y} at each \code{x} for when \code{what="byx"} and the \code{y} variable is continuous numeric, or set \code{byx.type="violin"} (the default) to plot half-violin plots at each \code{x}.
+#' @param violinbox set to \code{TRUE} to add violin plots to box plots
+#' @param violinbox.opts a list to pass to \code{panel.violin}
+#' @param fun a function that takes individual response variables (which may be matrices, as in \code{\link[survival]{Surv}} objects) and creates one or more summary statistics that will be computed while the resulting data frame is being collapsed to one row per condition.  Dot charts are drawn when \code{fun} is given.
+#' @param data data frame
+#' @param subset a subsetting epression for the entire analysis
+#' @param na.action a NA handling function for data frames, default is \code{na.retain}
+#' @param panel character string.  Name of panel, which goes into file base names and figure labels for cross-referencing
+#' @param subpanel If calling \code{dReport} more than once for the same type of chart (by different values of \code{what}), specify \code{subpanel} to distinguish the multiple calls.  In that case, \code{-subpanel} will be appended to \code{panel} when creating figure labels and cross-references.
+#' @param head character string.  Specifies initial text in the figure caption, otherwise a default is used
+#' @param tail optional character string.  Specifies final text in the figure caption, e.g., what might have been put in a footnote in an ordinary text page.  This appears just before any needles.
+#' @param continuous the minimum number of numeric values a variable must have in order to be considered continuous
+#' @param h numeric.  Height of plot, in inches
+#' @param w numeric.  Width of plot
+#' @param outerlabels logical that if \code{TRUE}, pass \code{lattice} graphics through the \code{latticeExtra} package's \code{useOuterStrips}function if there are two conditioning (paneling) variables, to put panel labels in outer margins.
+#' @param \dots. Passed to \code{summaryP} or \code{bpplotM}
+#' @param append logical.  Set to \code{FALSE} to start a new panel
+#' @param sopts list specifying extra arguments to pass to \code{bpplotM}, \code{summaryP}, or \code{summaryS}
+#' @param popts list specifying extra arguments to pass to a plot method.  One example is \code{text.at} to specify some number beyond \code{xlim[2]} to leave extra space for numerators and denominators when using \code{summaryP} for categorical analysis variables.  Another common use is for example \code{popts=list(layout=c(columns,rows))} to be used in rendering \code{lattice} plots.  \code{key} and \code{panel} are also frequently used.
+#' @export
+#' @examples
+#' # See test.Rnw in tests directory
+
+dReport <-
+  function(formula, groups=NULL,
+           what=c('box', 'proportions', 'xy', 'byx'),
+           byx.type=c('violin', 'quantiles'),
+           violinbox=TRUE,
+           violinbox.opts=list(col=adjustcolor('blue', alpha.f=.25),
+             border=FALSE),
+           fun=NULL, data=NULL, subset=NULL, na.action=na.retain,
+           panel = 'desc', subpanel=NULL, head=NULL, tail=NULL,
+           continuous=10, h=5.5, w=5.5, outerlabels=TRUE, append=FALSE,
+           sopts=NULL, popts=NULL)
+{
+  mwhat    <- missing(what)
+  what     <- match.arg(what)
+  byx.type <- match.arg(byx.type)
+  tvar     <- getgreportOption('tx.var')
+  
+  center <- 'centerline'
+  legend <- NULL
+
+  Nobs <- nobsY(formula, group=tvar,
+                data=data, subset=subset, na.action=na.action)
+  formula.no.id <- Nobs$formula   ## removes id()
+  form <- Formula(formula)
+  environment(form) <- new.env(parent = environment(form))
+  en <- environment(form)
+  assign(envir = en, 'id', function(x) x)
+
+  Y <- if(length(subset)) model.frame(form, data=data, subset=subset,
+                                      na.action=na.action)
+   else model.frame(form, data=data, na.action=na.action)
+  X <- model.part(form, data=Y, rhs=1)
+  Y <- model.part(form, data=Y, lhs=1)
+  rhs <- terms(form, rhs=1, specials='id')
+  sr  <- attr(rhs, 'specials')
+  ## specials counts from lhs variables
+  wid <- sr$id
+  if(length(wid)) wid <- wid - ncol(Y)
+  
+  if(mwhat) {
+    if(length(fun)) what <- 'xy'
+    else {
+      y <- Y[[1]]
+      if(is.character(y) || is.factor(y) || inherits(y, 'ynbind'))
+        what <- 'proportions' else type <- 'box'
+    }
+  }
+  
+  labs      <- sapply(X, label)
+  if(length(wid)) labs <- labs[- wid]
+  stratlabs <- ifelse(labs == '',
+                      if(length(wid)) names(X)[-wid] else names(X), labs)
+  ylabs     <- sapply(Y, label)
+  ylabs     <- ifelse(ylabs == '', names(Y), ylabs)
+
+  past <- function(x) {
+    l <- length(x)
+    if(l < 2) x
+    else if(l == 2) paste(x, collapse=' and ')
+    else paste(paste(x[1 : (l - 1)], collapse=', '), x[l], sep=', and ')
+  }
+
+  quant <- function(y) {
+    probs <- c(0.05, 0.125, 0.25, 0.375)
+    probs <- sort(c(probs, 1 - probs))
+    y <- y[! is.na(y)]
+    if(length(y) < 3) {
+      w <- c(median(y), rep(NA, 9), length(y))
+      names(w) <- c('Median', format(probs), 'se', 'n')
+      return(w)
+    }
+    w <- hdquantile(y, probs)
+    m <- hdquantile(y, 0.5, se=TRUE)
+    se <- as.numeric(attr(m, 'se'))
+    c(Median=as.numeric(m), w, se=se, n=length(y))
+  }
+
+  meanse <- function(y) {
+    y <- y[! is.na(y)]
+    n <- length(y)
+    se <- if(n < 2) NA else sd(y) / sqrt(n)
+    c(smean.cl.boot(y, na.rm=FALSE), se=se, n=length(y))
+  }
+
+  propw <- function(y) {
+    y <- y[!is.na(y)]
+    n <- length(y)
+    p <- mean(y)
+    se <- sqrt(p * (1. - p) / n)
+    structure(c(binconf(sum(y), n), se=se, n=n),
+              names=c('Proportion', 'Lower', 'Upper', 'se', 'n'))
+  }
+
+  latexit <- function(s, what, byx.type, file) {
+    at <- attributes(s)
+    xv <- at$xnames
+    ## panel function did the work:
+    if(what == 'byx.cont' && byx.type == 'violin') {
+      g <- function(y) {
+        y <- y[! is.na(y)]
+        if(length(y) < 3) 
+          return(c(n=length(y), Median=median(y), Q1=NA, Q3=NA))
+        w <- hdquantile(y, c(0.50, 0.25, 0.75))
+        r <- c(length(y), w)
+        names(r) <- c('n', 'Median', '0.250', '0.750')
+        r
+      }
+      ## Attempt to find a good number of digits to right of .
+      r <- min(tapply(s$y, s$yvar, function(x) max(abs(x), na.rm=TRUE)),
+               na.rm=TRUE)
+      dig <- if(r == 0) 2
+       else max(0, min(5, 3 - round(log10(r))))
+      
+      s <- with(s, summarize(y, s[c('yvar', xv)],
+                             g, type='matrix', keepcolnames=TRUE))
+    } else dig <- 2
+    sk <- switch(what,
+                 byx.cont = c(n='n', Median='Median', Q1='0.250', Q3='0.750'),
+                 byx.binary   = c(n='n', Proportion='Proportion'),
+                 byx.discrete = c(n='n', Mean='Mean', Lower='Lower',
+                   Upper='Upper'))
+    cround <- switch(what,
+                     byx.cont     = 2:4,
+                     byx.binary   = 2,
+                     byx.discrete = 2:4)
+
+    s$y <- s$y[, sk, drop=FALSE]
+ 
+    s$y[, cround] <- round(s$y[, cround], dig)
+    colnames(s$y) <- names(sk)
+    yv <- unique(as.character(s$yvar))
+    ny <- length(yv)
+    ylab <- character(ny)
+    names(ylab) <- yv
+    for(v in yv) ylab[v] <-
+      labelLatex(label=upFirst(at$ylabels[v]), units=at$yunits[v], hfill=TRUE)
+    
+    if(length(xv) == 2) {
+      r <- reshape(s, timevar=xv[2], direction='wide', idvar=c('yvar', xv[1]))
+      class(r) <- 'data.frame'
+      lev <- levels(s[[xv[2]]])
+      nl <- length(lev)
+      yvar <- unique(as.character(r$yvar))
+      w <- latex(r[colnames(r) != 'yvar'],
+                 table.env=FALSE, file=file, append=TRUE, rowlabel='',
+                 rowname=rep('', nrow(r)),
+                 cgroup=c('', lev),
+                 n.cgroup=c(1, rep(ncol(s$y), nl)),
+                 rgroup=ylab[yvar], size='scriptsize',
+                 colheads=c(upFirst(xv[1]), rep(names(sk), nl)), center=center)
+    }
+  else {
+    yvar <- unique(as.character(s$yvar))
+    w <- latex(s[colnames(s) != 'yvar'],
+               table.env=FALSE, file=file, append=TRUE,
+               rowlabel='', rowname=rep('', nrow(s)),
+               rgroup=ylab[yvar], size='small',
+               colheads=c(upFirst(xv[1]), names(sk)), center=center) 
+  }
+    if(length(xv) == 2) 'full' else 'mini'
+  }
+
+  if(what == 'byx') {
+    if(length(fun)) stop('may not specify fun= when what="byx"')
+    g <- function(y) {
+      if(is.logical(y)) 2
+      else if(! is.numeric(y)) 0
+      else length(unique(y[! is.na(y)]))
+    }
+    nu <- min(sapply(Y, g))
+    what <- if(nu < 3) {
+      fun <- propw
+      'byx.binary'
+    } else if(nu < continuous) {
+      fun <- meanse
+      'byx.discrete'
+    } else {
+      if(byx.type == 'quantiles') fun <- quant
+      'byx.cont'
+    }
+  }
+
+  file <- sprintf('%s/%s.tex', getgreportOption('texdir'), panel)
+  if(!append) cat('', file=file)
+
+  lb <- sprintf('%s-%s', panel, what)
+  if(length(subpanel)) lb <- paste(lb, subpanel, sep='-')
+  lbn <- gsub('\\.', '', gsub('-', '', lb))
+  lttpop <- paste('ltt', lbn, sep='')
+
+  ## Is first x variable on the x-axis of an x-y plot?
+  fx <- (what == 'xy' && ! length(fun)) || substring(what, 1, 3) == 'byx'
+  a <- if(fx) {
+    if(length(ylabs) < 7)
+      paste(if(what != 'xy') 'for', past(ylabs), 'vs.\\', stratlabs[1])
+     else paste('for', length(ylabs), 'variables vs.\\', stratlabs[1])
+  } else paste('for',
+               if(length(ylabs) < 7) past(ylabs) else
+               paste(length(ylabs), 'variables'))
+  al <- tolower(a)
+  
+  if(!length(head))
+    head <-
+      switch(what,
+       box          = paste('Extended box',
+         if(violinbox) 'and violin', 'plots', al),
+       proportions  = paste('Proportions', al),
+       xy           =  if(length(fun)) 'Statistics' else a,
+       byx.binary   = paste('Proportions and confidence limits', al),
+       byx.discrete =
+             paste('Means and 0.95 bootstrap percentile confidence limits', al),
+       byx.cont     = paste('Medians',
+         switch(byx.type, quantiles='with quantile intervals',
+                violin='with violin (density) plots'),
+         al)      )
+
+  sl <- tolower(past(if((what == 'xy' && ! length(fun)) || 
+                        what %in% c('byx.binary', 'byx.discrete',
+                                    'byx.cont'))
+                     stratlabs[-1] else stratlabs))
+  cap <- if(!length(sl)) head
+  else sprintf('%s stratified by %s', head, sl)
+
+  shortcap <- cap
+  tcap <- switch(what,
+                 box = paste('Statistics', al),
+                 proportions = paste('Proportions', al),
+                 xy = if(length(fun)) 'Statistics' else a,
+                 byx.binary=paste('Proportions and confidence limits', al),
+                 byx.discrete=paste('Means and 0.95 bootstrap CLs', al),
+                 byx.cont=paste('Medians', al))
+  tcap <- if(length(sl)) sprintf('%s stratified by %s', tcap, sl)
+  
+  cap <- gsub('Extended box', '\\\\protect\\\\eboxpopup{Extended box}', cap)
+  cap <- gsub('quantile intervals', '\\\\protect\\\\qintpopup{quantile intervals}',
+              cap)
+  
+  startPlot(lb, h=h, w=w)
+  dl <- list(formula=formula.no.id,
+             data=data, subset=subset, na.action=na.action,
+             outerlabels=outerlabels)
+  key <- popts$key
+  if(! length(key) && length(groups)) {
+    glevels <- levels(X[[groups]])
+    klines <- list(x=.6, y=-.07, cex=.8,
+                   columns=length(glevels), lines=TRUE, points=FALSE)
+    key=switch(what,
+      box = NULL,
+      proportions = list(columns=length(glevels),
+        x=.75, y=-.04, cex=.9,
+        col=trellis.par.get('superpose.symbol')$col, corner=c(0,1)),
+      xy = klines,
+      byx.binary =,
+      byx.discrete =,
+      byx.cont = klines)
+  }
+  if(length(key)) popts$key <- key
+
+  switch(what,
+         box = {
+           sopts$violin      <- violinbox
+           sopts$violin.opts <- violinbox.opts
+           s <- do.call('bpplotM', c(dl, sopts))
+           print(s)
+         },
+         proportions = {
+           s <- do.call('summaryP', c(dl, sopts))
+           p <- do.call('plot', c(list(x=s, groups=groups), popts))
+           print(p)
+         },
+         xy = {
+           s <- do.call('summaryS', c(dl, list(fun=fun), sopts))
+           p <- do.call('plot', c(list(x=s, groups=groups), popts))
+           print(p)
+         },
+         byx.binary = ,
+         byx.discrete =,
+         byx.cont = {
+           s <- do.call('summaryS', c(dl, list(fun=fun), sopts))
+           ylim <- NULL
+           if(what %in% c('byx.binary', 'byx.discrete') &&
+              all(c('Lower', 'Upper') %in% colnames(s$y))) {
+             yvl <- levels(s$yvar)
+             ylim <- vector('list', length(yvl))
+             names(ylim) <- yvl
+             for(yv in levels(s$yvar)) {
+               j <- s$yvar == yv
+               ylim[[yv]] <- c(min(s$y[j, 'Lower'], na.rm=TRUE),
+                               max(s$y[j, 'Upper'], na.rm=TRUE))
+             }
+           }
+           p <- do.call('plot',
+            c(list(x=s, groups=groups, ylim=ylim,
+                   panel=if(byx.type == 'violin' && what == 'byx.cont')
+                         medvPanel else mbarclPanel,
+                   paneldoesgroups=TRUE), popts))
+           print(p)
+         } )
+
+  popname <- paste('poptable', lbn, sep='')
+  cat(sprintf('\\def\\%s{\\protect\n', popname), file=file, append=TRUE)
+  poptab <- NULL
+
+  if(substring(what, 1, 3) == 'byx')
+    poptab <- latexit(s, what, byx.type, file=file)
+  else if(what == 'proportions') {
+    z <- latex(s, groups=groups, size='small', file=file)
+    poptab <- if(attr(z, 'ngrouplevels') > 3) 'full' else 'mini'
+  }
+  else if(what == 'box' || (what == 'xy' && length(fun))) {
+    S <- summaryM(formula.no.id, data=data, subset=subset, na.action=na.action,
+                  test=FALSE, groups=groups)
+    z <- latex(S, table.env=FALSE, file=file, append=TRUE, prmsd=TRUE,
+               npct='both', exclude1=FALSE, middle.bold=TRUE, center=center,
+               round='auto', insert.bottom=FALSE)
+    poptab <- if(length(S$group.freq) > 3) 'full' else 'mini'
+    legend <- attr(z, 'legend')
+    legend <- if(! length(legend)) ''
+     else paste('. ', paste(legend, collapse='\n'), sep='')
+  }
+  cat('}\n', file=file, append=TRUE)
+  
+  nobs <- Nobs$nobs
+  r <- range(nobs)
+  nn <- if(r[1] == r[2]) r[1] else paste(r[1], 'to', r[2])
+  cap <- sprintf('%s. $N$=%s', cap, nn)
+  if(length(tail)) cap <- paste(cap, tail, sep='. ')
+  n <- c(randomized=r[2])
+  nobsg <- Nobs$nobsg
+  if(length(nobsg)) n <- c(n, apply(nobsg, 1, max))
+  dNeedle(sampleFrac(n, nobsY=Nobs), name=lttpop, file=file)
+  cap <- sprintf('%s~\\hfill\\%s', cap, lttpop)
+
+  endPlot()
+    
+  putFig(panel = panel, name = lb, caption = shortcap,
+         longcaption = cap,  tcaption=tcap,
+         tlongcaption = paste(tcap, legend, sep=''),
+         poptable= if(length(poptab)) paste('\\', popname, sep=''),
+         popfull = length(poptab) && poptab == 'full')
+}
---
+++ R/survReport.r
@@ -0,0 +1,177 @@
+#' Survival Report
+#'
+#' Generate a Survival Report with Kaplan-Meier Estimates
+#'
+#' @param formula a formula with survival (\code{Srv}) objects on the left hand side and an optional stratification factor on the right (or \code{1} if none).  The survival object component variables should be labeled; these labels are used for graph annotation.
+#' @param data data.frame
+#' @param subset optional subsetting criteria
+#' @param na.action function for handling \code{NA}s while creating a data frame
+#' @param ylab character. Passed to \code{\link[rms]{survplot.survfit}} as the \code{ylab} argument.  Constructed by default.
+#' @param what \code{"S"} (the default) to plot survival functions or \code{"1-S"} to plot cumulative incidence functions.
+#' @param conf character. See \code{\link[rms]{survplot.survfit}}.
+#' @param panel character string.  Name of panel, which goes into file base names and figure labels for cross-referencing.
+#' @param subpanel character string.  If calling \code{dReport} more than once for the same type of chart (categorical or continuous), specify \code{subpanel} to distinguish the multiple calls.  In that case, \code{-subpanel} will be appended to \code{panel} when creating figure labels and cross-references.
+#' @param head character string.  Specifies initial text in the figure caption, otherwise a default is used.
+#' @param tail optional character string.  Specifies final text in the figure caption, e.g., what might have been put in a footnote in an ordinary text page.  This appears just before any needles.
+#' @param h numeric. Height of plots.
+#' @param w numeric. Width of plots in inches.
+#' @param multi logical.  If \code{TRUE}, multiple figures are produced, othewise a single figure with a matrix of survival plots is made.
+#' @param mfrow numeric 2-vector, used if \code{multi=FALSE}.  If not specified, default plot matrix layout will be figured.
+#' @param y.n.risk used if \code{what="1-S"}, to specify \code{y} coordinate for putting numbers at risk, typically below the \code{x}-axis label
+#' @param bot number of spaces to reserve at bottom of plot for numbers at risk, if \code{what="1-S"}
+#' @param append logical. If \code{TRUE} output will be appended instead of overwritten.
+#' @param \dots ignored
+#' @export
+#' @examples
+#' ## See tests directory test.Rnw for a live example
+#' \dontrun{
+#'   set.seed(1)
+#'   n <- 400
+#'   dat <- data.frame(t1=runif(n, 2, 5), t2=runif(n, 2, 5),
+#'                     e1=rbinom(n, 1, .5), e2=rbinom(n, 1, .5),
+#'                     treat=sample(c('a','b','c'), n, TRUE))
+#'   dat <- upData(dat,
+#'                 labels=c(t1='Time to operation',
+#'                          t2='Time to rehospitalization',
+#'                          e1='Operation', e2='Hospitalization',
+#'                          treat='Treatment')
+#'                 units=c(t1='year', t2='year'))
+#'   survReport(Srv(t1, e1) + Srv(t2, e2) ~ treat, data=dat)
+#' }
+
+survReport <- function(formula, data=NULL, subset=NULL, na.action=na.retain,
+                       ylab=NULL, what=c('S', '1-S'),
+                       conf=c('diffbands', 'bands', 'bars', 'none'),
+                       panel='surv', subpanel=NULL, head=NULL, tail=NULL,
+                       h=3, w=4.5, multi=FALSE, mfrow=NULL, y.n.risk=-.5,
+                       bot=2, append=FALSE, ...)
+{
+  what <- match.arg(what)
+  conf <- match.arg(conf)
+  form <- Formula(formula)
+  Y <- if(length(subset)) model.frame(form, data=data, subset=subset,
+                                      na.action=na.action)
+  else model.frame(form, data=data, na.action=na.action)
+  X <- model.part(form, data=Y, rhs=1)
+  Y <- model.part(form, data=Y, lhs=1)
+  
+  conf <- match.arg(conf)
+
+  texdir <- getgreportOption('texdir')
+
+  if(! append) cat('', file=sprintf('%s/%s.tex', texdir, panel))
+  lb <- if(length(subpanel)) sprintf('%s-%s', panel, subpanel)
+   else panel
+
+  namx <- labx <- NULL
+  if(length(X)) {
+    x <- X[[1]]
+    namx <- names(X)[1]
+    labx  <- tolower(ifelse(label(x) == '', namx, label(x)))
+  }
+
+  Nobs <- nobsY(formula, group=getgreportOption('tx.var'),
+                data=data, subset=subset, na.action=na.action)
+
+  ny <- ncol(Y)
+  if(ny == 1) multi <- FALSE
+  
+  if(! multi) {
+    mf <- if(length(mfrow)) mfrow else mfrowSuggest(ny)
+    startPlot(lb, h=h, w=w, mfrow=mf, bot=if(what=='1-S') bot else 0,
+              lattice=FALSE, ...)
+  }
+
+  gro <- getgreportOption()
+  x.is.tx <- FALSE; ng <- 0
+  if(length(X)) {
+    x <- X[[1]]
+    ng <- if(is.factor(x)) length(levels(x)) else
+     length(unique(x[!is.na(x)]))
+    if(namx == gro$tx.var) {
+      x.is.tx <- TRUE
+      col <- gro$tx.linecol
+      lwd <- gro$tx.lwd
+    }
+    else {
+      col <- rep(c(gray(c(0, .7)), 'blue', 'red', 'green'), length=ng)
+      lwd <- rep(c(1, 3), length=ng)
+    }
+  } else {
+    x <- rep('', nrow(Y))
+    col <- 1
+    lwd <- 2
+  }
+
+  nobs <- rep(0, 1 + x.is.tx * ng)
+  evlab <- character(ny)
+  for(i in 1 : ny) {
+    y <- Y[[i]]
+    evlab[i] <- tolower(label(y))
+    no <- nrow(y[! is.na(y)])
+    if(multi) {
+      lbi <- paste(lb, i, sep='-')
+      startPlot(lbi, h=h, w=w, bot=if(what=='1-S') bot else 0, lattice=FALSE, ...)
+    }
+    yl <- if(length(ylab)) ylab else {
+      yl <- upFirst(evlab[i])
+      if(what == 'S') paste(yl, '-Free Probability', sep='')
+       else paste('Incidence of', yl)
+    }
+    s <- survfit(y ~ x)
+    if(x.is.tx) {
+      no        <- c(no, s$n)
+      names(no) <- c('randomized', levels(x))
+    }
+    if(what == 'S')
+      survplot(s, 
+               n.risk=TRUE, conf=conf, lwd=lwd,
+               lty=1, col=col, ylab=yl,
+               label.curves=list(keys='lines', key.opts=list(bty='n')),
+               levels.only=TRUE, ...)
+    else
+      survplot(s, fun=function(y) 1 - y,
+               n.risk=TRUE, y.n.risk=y.n.risk, conf=conf, lwd=lwd,
+               lty=1, col=col, ylab=yl,
+               label.curves=list(keys='lines', key.opts=list(bty='n')),
+               levels.only=TRUE, ...)
+    
+    if(multi) {
+      endPlot()
+      shortcap <- if(length(head)) head else 'Kaplan-Meier estimates'
+      shortcap <- paste(shortcap, 'for', evlab[i])
+      if(length(labx))
+        shortcap <- paste(shortcap, 'stratified by', labx)
+      cap <- paste(shortcap,
+                   ', along with half-height of 0.95 confidence limits centered at survival estimate midpoints. $N$=', no[1],
+                   '. ', tail, sep='')
+      dNeedle(sampleFrac(no, Nobs), name='lttsurv',
+              file=sprintf('%s/%s.tex', texdir, panel))
+      cap <- sprintf('%s~\\hfill\\lttsurv', cap)
+      putFig(panel=panel, name=lbi, caption=shortcap, longcaption=cap)
+    }
+    if(! multi) for(j in 1:length(nobs)) nobs[j] <- max(nobs[j], no[j])
+    names(nobs) <- names(no)
+  }
+  
+  if(! multi) {
+    past <- function(x) {
+      l <- length(x)
+      if(l < 2) x
+      else if(l == 2) paste(x, collapse=' and ')
+      else paste(paste(x[1 : (l - 1)], collapse=', '), x[l], sep=', and ')
+    }
+
+    endPlot()
+    shortcap <- if(length(head)) head else 'Kaplan-Meier estimates'
+    shortcap <- paste(shortcap, 'for', past(evlab))
+    if(length(labx))
+      shortcap <- paste(shortcap, 'stratified by', labx)
+    cap <- paste(shortcap,
+                 ', along with half-height of 0.95 confidence limits centered at survival estimate midpoints. $N$=', nobs[1], '. ', tail, sep='')
+    dNeedle(sampleFrac(nobs, Nobs), name='lttsurv',
+            file=sprintf('%s/%s.tex', texdir, panel))
+    cap <- sprintf('%s~\\hfill\\lttsurv', cap)
+    putFig(panel=panel, name=lb, caption=shortcap, longcaption=cap)
+  }
+}
