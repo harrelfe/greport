8ed6e36c0a4ba4545db051efe54da2ad1520974c
harrelfe <f.harrell@vanderbilt.edu>
2014-03-02 22:26:59 -0600

    survReport: prepare for use of npsurv in place of summary.formula

--- R/survReport.r
+++ R/survReport.r
@@ -118,7 +118,32 @@ survReport <- function(formula, data=NULL, subset=NULL, na.action=na.retain,
       if(what == 'S') paste(yl, '-Free Probability', sep='')
        else paste('Incidence of', yl)
     }
-    s <- survfit(y ~ x)
+
+    # The following function is built-in in next release of rms
+    ## TODO
+if(!existsFunction('npsurv')) {
+  npsurv <- function(formula, data, subset, na.action, ...) {
+    m <- match.call(expand.dots = FALSE)
+    m$... <- NULL
+    m[[1]] <- as.name('survfit')
+    m$formula <- formula
+    f <- eval(m, sys.parent())
+    m[[1]] <- as.name('model.frame')
+    g <- eval(m, sys.parent())
+    
+    f$maxtime <- max(f$time)
+    
+    Y <- model.extract(g, 'response')
+    f$units <- units(Y)
+    f$time.label  <- label(Y, type='time')
+    f$event.label <- label(Y, type='event')
+    f$call <- match.call()
+    class(f) <- c('npsurv', 'survfit')
+    f
+  }
+  survplot.npsurv <- rms:::survplot.survfit
+}
+    s <- npsurv(y ~ x)
     if(x.is.tx) {
       no        <- c(no, s$n)
       names(no) <- c('randomized', levels(x))
