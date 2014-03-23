2273721f74bcc024c432337e36db90932fd8eaa0
harrelfe <f.harrell@vanderbilt.edu>
2014-03-23 13:09:32 -0500

    roxygenized NAMESPACE

---
+++ R/greport-package.r
@@ -0,0 +1,18 @@
+#' Graphical Reporting for Clinical Trials
+#'
+#' @author Frank E Harrell Jr \email{f.harrell@@vanderbilt.edu}
+#'
+#' @maintainer Frank E Harrell Jr \email{f.harrell@@vanderbilt.edu}
+#'
+#' @export Merge accrualReport dNeedle dReport eReport endPlot exReport getgreportOption nriskReport putFig sampleFrac setgreportOption startPlot survReport upFirst
+#' @import Hmisc lattice latticeExtra data.table
+#' @importFrom rms npsurv survplot
+#' @importFrom survival Surv survfit
+#' @importFrom Formula Formula model.part
+#' @docType package
+#' @aliases greport package-greport
+#' @name greport
+
+# The caching and check for conflicts require looking for a pattern of objects; the search may be avoided by defining an object ‘.noGenerics’
+# see ?library
+.noGenerics <- TRUE
