9fff4d4d6144c5ece3ccd33a9c2b47d255154037
harrelfe <f.harrell@vanderbilt.edu>
2014-03-23 12:26:00 -0500

    Merge: fix examples for data.table

--- R/Misc.r
+++ R/Misc.r
@@ -437,8 +437,12 @@ upFirst <- function(txt) {
 #' b <- data.frame(sid=c(1,2,2), bp=c(120,130,140))
 #' d <- data.frame(sid=c(1,3,4), wt=c(170,180,190))
 #' all <- Merge(a, b, d, id = ~ sid)
+#' # For data.table, first file must be the master file and must
+#' # contain all ids that ever occur.  ids not in the master will
+#' # not be merged from other datasets.
 #' a <- data.table(a); setkey(a, sid)
-#' b <- data.table(b); setkey(b, sid)
+#' # data.table also does not allow duplicates without allow.cartesian=TRUE
+#' b <- data.table(sid=1:2, bp=c(120,130)); setkey(b, sid)
 #' d <- data.table(d); setkey(d, sid)
 #' all <- Merge(a, b, d)
 
